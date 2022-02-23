786f2cf5bf261ee55b4388730b11254fdb6dbc09
lmirenda <lucamateomirenda@gmail.com>
2022-02-23 09:43:24 -0300

    Create Petition model.

--- app/Models/Petition.php
+++ app/Models/Petition.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Petition extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title','description','doctor_id','patient_id','status'];
+
+    protected $with = ['patient', 'doctor'];
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class,'id');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class,'id');
+    }
 }
