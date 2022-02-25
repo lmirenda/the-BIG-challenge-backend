bb737683282a19c5a3a0a7f6a64b616416e812b9
lmirenda <lucamateomirenda@gmail.com>
2022-02-25 16:11:50 -0300

    Create and test User Login.

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController
+{
+    public function __invoke(LoginRequest $request): JsonResponse
+    {
+        if(Auth::attempt($request->only('email','password')))
+        {
+            return response()->json(['Welcome']);
+        }
+        return response()->json(['Wrong email and/or password'],422);
+    }
+}
