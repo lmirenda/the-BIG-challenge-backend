8f8505bf635b7869fdd07d189c197e079a263146
lmirenda <lucamateomirenda@gmail.com>
2022-02-28 11:58:53 -0300

    Config password and account type rules.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController
 {
     public function __invoke(RegisterRequest $request): JsonResponse
     {
-        $request->get('key');
-
-//          creo usario
-//          disapro evento usuario registrado
-
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'type' => $request->get('type')
+        ]);
+        //TODO: Trigger event
+        return response()->json(['Registration Successful']);
     }
 }
