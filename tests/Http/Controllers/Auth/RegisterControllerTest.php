49711f6d0238e734889a58481ee7970df4e60ab3
lmirenda <lucamateomirenda@gmail.com>
2022-03-02 09:08:17 -0300

    Add data providers to Registration Test.

--- tests/Http/Controllers/Auth/RegisterControllerTest.php
+++ tests/Http/Controllers/Auth/RegisterControllerTest.php
@@ -15,179 +15,119 @@ class RegisterControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_new_user_can_be_registered()
+    /**
+     * @dataProvider  validUserDataProvider
+     */
+    public function test_new_user_can_be_registered($userData)
     {
         Event::fake();
-
-        $userData = [
-            'name' => 'John Doe',
-            'email' => 'john@doe.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-
         $this
             ->postJson('api/register', $userData)
             ->assertSuccessful();
-    }
-
-    public function test_successful_registration_dispatches_event()
-    {
-        Event::fake();
-        $userData = [
-            'name' => 'John Doe',
-            'email' => 'john@doe.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-
-        $this
-            ->postJson('api/register', $userData);
-
         Event::assertDispatched(UserHasRegistered::class);
     }
 
-    public function test_successful_registration_sends_one_email()
+    /**
+     * @dataProvider  validUserDataProvider
+     */
+    public function test_successful_registration_sends_one_email($userData)
     {
         Mail::fake();
-        $userData = [
-            'name' => 'John Doe',
-            'email' => 'john@doe.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-
         $this
             ->postJson('api/register', $userData);
 
-        Mail::assertSent(RegisterNewUserMail::class);
+        Mail::assertSent(RegisterNewUserMail::class, 1);
     }
 
-    public function test_new_user_cant_register_without_matching_passwords()
+    /**
+     * @dataProvider  invalidUserDataProvider
+     */
+    public function test_new_user_cant_register_with_invalid_data($user)
     {
-        $userData = [
-            'name' => 'John Doe',
-            'email' => 'john@doe.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password02',
-            'type' => Random::userType(),
-        ];
-
-        $this
-            ->postJson('api/register', $userData)
-            ->assertUnprocessable();
-    }
-
-    public function test_new_user_cant_register_with_used_email()
-    {
-        User::factory()->create(['email'=>'test@email.io']);
-        $userData = [
-            'name' => 'John Doe',
-            'email'=>'test@email.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-
-        $this
-            ->postJson('api/register', $userData)
-            ->assertUnprocessable();
-    }
-
-    public function test_new_user_cant_register_with_unknown_type()
-    {
-        $userData = [
-            'name' => 'John Doe',
-            'email'=>'test@email.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => 'other',
-        ];
-        $this
-            ->postJson('api/register', $userData)
-            ->assertUnprocessable();
-    }
-
-    public function test_new_user_cant_register_with_short_name()
-    {
-        $userData = [
-            'name' => 'Do',
-            'email'=>'test@email.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-        $this
-            ->postJson('api/register', $userData)
-            ->assertUnprocessable();
-    }
-
-    public function test_new_user_cant_register_with_empty_fields()
-    {
-        $noName = [
-            'email'=>'test@email.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-        $noEmail = [
-            'name' => 'John Doe',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-        $noPassword = [
-            'name' => 'John Doe',
-            'email'=>'test@email.io',
-            'confirmed_password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-        $noConfirmation = [
-            'name' => 'John Doe',
-            'email'=>'test@email.io',
-            'password' => 'Password01',
-            'type' => Random::userType(),
-        ];
-        $noType = [
-            'name' => 'John Doe',
-            'email'=>'test@email.io',
-            'password' => 'Password01',
-            'confirmed_password' => 'Password01',
-        ];
-
-        $this
-            ->postJson('api/register', $noName)
-            ->assertUnprocessable();
-        $this
-            ->postJson('api/register', $noEmail)
-            ->assertUnprocessable();
-        $this
-            ->postJson('api/register', $noPassword)
-            ->assertUnprocessable();
-        $this
-            ->postJson('api/register', $noConfirmation)
-            ->assertUnprocessable();
-        $this
-            ->postJson('api/register', $noType)
-            ->assertUnprocessable();
-    }
-
-    public function test_failed_registration_does_not_dispatch_event()
-    {
+        User::factory()->create(['email'=>'repeated@email.io']);
         Event::fake();
-        $this
-            ->postJson('api/register', []);
-        Event::assertNotDispatched(UserHasRegistered::class);
-    }
-
-    public function test_failed_registration_does_not_send_email()
-    {
         Mail::fake();
         $this
-            ->postJson('api/register', []);
+            ->postJson('api/register', $user)
+            ->assertUnprocessable();
+        Event::assertNotDispatched(UserHasRegistered::class);
         Mail::assertNothingSent();
+    }
+
+    public function validUserDataProvider(): array
+    {
+        return [
+            ['User data' => [
+                    'name' => 'John Doe',
+                    'email' => 'john@doe.io',
+                    'password' => 'Password01',
+                    'confirmed_password' => 'Password01',
+                    'type' => Random::userType(),
+            ]]
+        ];
+    }
+
+    public function invalidUserDataProvider() :array
+    {
+        return [
+            ['no Name' => [
+                'email'=>'test@email.io',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password01',
+                'type' => Random::userType(),
+            ]],
+            ['no Email' => [
+                'name' => 'John Doe',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password01',
+                'type' => Random::userType(),
+            ]],
+            ['no Password' => [
+                'name' => 'John Doe',
+                'email'=>'test@email.io',
+                'confirmed_password' => 'Password01',
+                'type' => Random::userType(),
+            ]],
+            ['no Confirmation' => [
+                'name' => 'John Doe',
+                'email'=>'test@email.io',
+                'password' => 'Password01',
+                'type' => Random::userType(),
+            ]],
+            ['no Type' => [
+                'name' => 'John Doe',
+                'email'=>'test@email.io',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password01',
+            ]],
+            ['other Type' => [
+                'name' => 'John Doe',
+                'email'=>'test@email.io',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password01',
+                'type' => 'other'
+            ]],
+            ['short Name' => [
+                'name' => 'Do',
+                'email'=>'test@email.io',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password01',
+                'type' => Random::userType(),
+            ]],
+            ['wrong Confirmation' => [
+                'name' => 'Do',
+                'email'=>'test@email.io',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password02',
+                'type' => Random::userType(),
+            ]],
+            ['repeated email' => [
+                'name' => 'Do',
+                'email'=>'repeated@email.io',
+                'password' => 'Password01',
+                'confirmed_password' => 'Password02',
+                'type' => Random::userType(),
+            ]],
+        ];
     }
 }
