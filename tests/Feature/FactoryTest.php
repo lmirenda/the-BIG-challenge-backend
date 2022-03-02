428a36cb53b0550c90bce5ac3ed9c35d5b7ddfec
lmirenda <lucamateomirenda@gmail.com>
2022-03-02 16:34:22 -0300

    Adjust user factory test.

--- tests/Feature/FactoryTest.php
+++ tests/Feature/FactoryTest.php
@@ -2,22 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Enums\UserType;
 use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class FactoryTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_the_user_factory_states_for_patient_and_doctor()
+    public function test_the_user_factory_states_for_patient()
     {
-        $patient = Patient::factory()
-           ->for(User::factory()->state([
-               'name' => 'Test Patient',
-           ]))
-           ->create();
-        $this->assertModelExists($patient);
+        $rolePatient = Role::create(['name' => UserType::PATIENT]);
+        $patient = Patient::factory()->create();
+        $this->assertDatabaseHas('patients', ['id' => $patient['id']]);
     }
 }
