77dd395d2e43febc8e2b934b4136780b93ed2ce7
lmirenda <lucamateomirenda@gmail.com>
2022-03-07 10:52:17 -0300

    Create routing for finishing petitions.

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Petitions\Doctor\AcceptPetitionController;
 use App\Http\Controllers\Petitions\Doctor\DoctorAcceptedPetitionsIndex;
+use App\Http\Controllers\Petitions\Doctor\FinishPetitionController;
 use App\Http\Controllers\Petitions\Doctor\PendingPetitionsIndex;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -31,3 +32,5 @@ Route::middleware(['role:doctor'])
     ->get('/petitions/accepted', DoctorAcceptedPetitionsIndex::class);
 Route::middleware(['role:doctor'])
     ->put('petitions/accept/{petition}', AcceptPetitionController::class);
+Route::middleware(['role:doctor'])
+    ->put('petitions/accepted/finish/{petition}', FinishPetitionController::class);
