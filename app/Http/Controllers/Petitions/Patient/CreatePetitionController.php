f0a261e9a6cf8683ba2bd473f8603f740c83912c
lmirenda <lucamateomirenda@gmail.com>
2022-03-08 10:31:24 -0300

    Create petitions controller, form request and testing.

---
+++ app/Http/Controllers/Petitions/Patient/CreatePetitionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Petitions\Patient;
+
+use App\Enums\PetitionStatus;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePetitionRequest;
+use App\Models\Petition;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CreatePetitionController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param CreatePetitionRequest $request
+     * @return JsonResponse
+     */
+    public function __invoke(CreatePetitionRequest $request): JsonResponse
+    {
+        $user = User::with('patientInformation.petitions')
+            ->where('id', Auth::user()->id)
+            ->first();
+
+        $petition = Petition::create([
+            'title' => $request->get('title'),
+            'symptoms' => $request->get('symptoms'),
+            'patient_id' => $user->patientInformation->id,
+            'doctor_id' => null,
+            'status' => PetitionStatus::PENDING->value
+        ]);
+        return response()->json([$petition]);
+    }
+}
--- tests/Http/Controllers/Petitions/Patient/CreatePetitionControllerTest.php
+++ tests/Http/Controllers/Petitions/Patient/CreatePetitionControllerTest.php
@@ -2,10 +2,68 @@
 
 namespace Tests\Http\Controllers\Petitions\Patient;
 
-use App\Http\Controllers\Petitions\Patient\CreatePetitionController;
-use PHPUnit\Framework\TestCase;
+use App\Enums\UserType;
+use App\Models\Patient;
+use App\Models\User;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
 
 class CreatePetitionControllerTest extends TestCase
 {
+    use RefreshDatabase;
 
+    public function test_patient_can_create_a_petition()
+    {
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()
+            ->patient()
+            ->create(['password'=>Hash::make(123456)])
+            ->assignRole(UserType::PATIENT->value);
+        Auth::attempt(['email'=>$user->email,'password'=>123456]);
+        Patient::factory()->create(['user_id'=>$user->id]);
+        $petitionData = [
+            'title' => 'Test title.',
+            'symptoms' => 'This are the symptoms.'
+        ];
+        $this
+            ->assertAuthenticated()
+            ->postJson('api/petitions/create', $petitionData)
+            ->assertSuccessful();
+    }
+    public function test_doctor_cant_create_a_petition()
+    {
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()
+            ->doctor()
+            ->create(['password'=>Hash::make(123456)])
+            ->assignRole(UserType::DOCTOR->value);
+        Auth::attempt(['email'=>$user->email,'password'=>123456]);
+        $petitionData = [
+            'title' => 'Test title.',
+            'symptoms' => 'This are the symptoms.'
+        ];
+        $this
+            ->assertAuthenticated()
+            ->postJson('api/petitions/create', $petitionData)
+            ->assertForbidden();
+    }
+    public function test_logged_out_patient_cant_create_a_petition()
+    {
+        $this->seed(RoleSeeder::class);
+        $user = User::factory()
+            ->patient()
+            ->create(['password'=>Hash::make(123456)])
+            ->assignRole(UserType::PATIENT->value);
+        Patient::factory()->create(['user_id'=>$user->id]);
+        $petitionData = [
+            'title' => 'Test title.',
+            'symptoms' => 'This are the symptoms.'
+        ];
+        $this
+            ->postJson('api/petitions/create', $petitionData)
+            ->assertForbidden();
+    }
 }
