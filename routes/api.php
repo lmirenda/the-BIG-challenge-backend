b30b8237ee2d9a36246538af6e2fe90a40bde809
lmirenda <lucamateomirenda@gmail.com>
2022-03-08 10:29:41 -0300

    Create petitions controller routing.

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Petitions\Doctor\AcceptPetitionController;
 use App\Http\Controllers\Petitions\Doctor\DoctorAcceptedPetitionsIndex;
 use App\Http\Controllers\Petitions\Doctor\FinishPetitionController;
 use App\Http\Controllers\Petitions\Doctor\PendingPetitionsIndex;
+use App\Http\Controllers\Petitions\Patient\CreatePetitionController;
 use App\Http\Controllers\Petitions\Patient\PetitionsIndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -33,7 +34,9 @@ Route::middleware(['role:doctor'])
     ->get('/petitions/accepted', DoctorAcceptedPetitionsIndex::class);
 Route::middleware(['role:doctor'])
     ->put('petitions/accept/{petition}', AcceptPetitionController::class);
-Route::middleware(['role:patient'])
-    ->get('/my/petitions', PetitionsIndexController::class);
 Route::middleware(['role:doctor'])
     ->put('petitions/accepted/finish/{petition}', FinishPetitionController::class);
+Route::middleware(['role:patient'])
+    ->get('/my/petitions', PetitionsIndexController::class);
+Route::middleware(['role:patient'])
+    ->post('/petitions/create', CreatePetitionController::class);
