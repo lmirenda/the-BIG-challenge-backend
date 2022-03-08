751a4f04ac7811bfeafa09e96b238f18fd894c40
lmirenda <lucamateomirenda@gmail.com>
2022-03-08 10:28:15 -0300

    Update model entity names.

---
+++ tests/Http/Controllers/Petitions/Patient/CreatePetitionControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Http\Controllers\Petitions\Patient;
+
+use App\Http\Controllers\Petitions\Patient\CreatePetitionController;
+use PHPUnit\Framework\TestCase;
+
+class CreatePetitionControllerTest extends TestCase
+{
+
+}
