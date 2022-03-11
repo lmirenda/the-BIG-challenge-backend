989c92eeaa646c51a5f6eebaf63da27a5871554e
lmirenda <lucamateomirenda@gmail.com>
2022-03-11 10:14:07 -0300

    Process request only on successful file upload.

--- app/Http/Controllers/Petitions/Doctor/FinishPetitionController.php
+++ app/Http/Controllers/Petitions/Doctor/FinishPetitionController.php
@@ -8,18 +8,29 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\FinishPetitionRequest;
 use App\Models\Petition;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
 
 class FinishPetitionController extends Controller
 {
     public function __invoke(Petition $petition, FinishPetitionRequest $request): JsonResponse
     {
-        $petition->update([
-            'status' => PetitionStatus::FINISHED->value,
-        ]);
-        $user = $petition->patient->user;
+        if ($request->hasFile('file')) {
 
-        event(new DoctorHasResponded($user));
+            $fileName = Str::random(16) . time() . ".txt";
+            $request
+                ->file('file')
+                ->storeAs('public/petition_files', $fileName);
 
-        return response()->json([$petition]);
+            $petition->update([
+                'status' => PetitionStatus::FINISHED->value,
+                'file' => $fileName
+            ]);
+            $user = $petition->patient->user;
+
+            event(new DoctorHasResponded($user));
+
+            return response()->json([$petition]);
+        }
+        return response()->json(['error']);
     }
 }
