b2dc54077e1de914c09f94888b172490555183b6
lmirenda <lmirenda@users.noreply.github.com>
2022-03-22 12:19:49 +0000

    Fix styling

--- tests/Http/Controllers/LogoutControllerTest.php
+++ tests/Http/Controllers/LogoutControllerTest.php
@@ -2,10 +2,8 @@
 
 namespace Tests\Http\Controllers;
 
-use App\Http\Controllers\LogoutController;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
