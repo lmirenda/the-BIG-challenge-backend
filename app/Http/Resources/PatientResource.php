53f62d824f64f4026586a356a5aa66601ad4e2ce
lmirenda <lucamateomirenda@gmail.com>
2022-03-25 09:51:19 -0300

    Create eloquen resoruce for controller

---
+++ app/Http/Resources/PatientResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Patient;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PatientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'user_id' => $this->user_id,
+            'patient_height' => $this->patient_height,
+            'patient_weight' => $this->patient_weight,
+            'patient_phone' => $this->patient_phone,
+            'patient_other_info' => $this->patient_other_info
+        ];
+    }
+}
