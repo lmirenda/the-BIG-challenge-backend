5fea52f99a67178f0a1ea52e43d350cc167be48d
lmirenda <lucamateomirenda@gmail.com>
2022-03-25 09:49:59 -0300

    Create patient controller

---
+++ app/Http/Controllers/Petitions/Patient/CreatePatientController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Petitions\Patient;
+
+use App\Enums\PetitionStatus;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePatientRequest;
+use App\Http\Resources\PatientResource;
+use App\Models\Patient;
+use Illuminate\Http\JsonResponse;
+
+class CreatePatientController extends Controller
+{
+    public function __invoke(CreatePatientRequest $request): PatientResource
+    {
+        $patient = Patient::create([
+            'user_id' => $request->user()->id,
+            'patient_height' => $request->get('patient_height'),
+            'patient_weight' => $request->get('patient_weight'),
+            'patient_phone' => $request->get('patient_phone'),
+            'patient_other_info' => $request->get('patient_other_info')
+        ]);
+        return new PatientResource($patient);
+    }
+}
