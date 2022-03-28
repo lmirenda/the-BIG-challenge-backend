9a08439104811f12e7f973f9d321f482f3b8616a
lmirenda <lucamateomirenda@gmail.com>
2022-03-28 08:58:28 -0300

    Ignore phpstan errors on resource return

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -15,10 +15,15 @@ class PatientResource extends JsonResource
     public function toArray($request)
     {
         return [
+            /** @phpstan-ignore-next-line */
             'user_id' => $this->user_id,
+            /** @phpstan-ignore-next-line */
             'patient_height' => $this->patient_height,
+            /** @phpstan-ignore-next-line */
             'patient_weight' => $this->patient_weight,
+            /** @phpstan-ignore-next-line */
             'patient_phone' => $this->patient_phone,
+            /** @phpstan-ignore-next-line */
             'patient_other_info' => $this->patient_other_info,
         ];
     }
